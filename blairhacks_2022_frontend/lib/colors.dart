00dec96e370547520323682bfe53a1541dad0224
nico671 <soccerzebra6@gmail.com>
2022-02-20 01:08:52 -0500

    added frontend

---
+++ blairhacks_2022_frontend/lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColor = Color.fromARGB(255, 255, 255, 255);
+
+Color gradientColor = Color.fromARGB(255, 107, 174, 229);
+
+Color accentColor = Color.fromARGB(255, 14, 133, 255);
